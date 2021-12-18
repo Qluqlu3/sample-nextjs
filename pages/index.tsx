3e4b29a46d59a106b45627343f33b4fc7401a0c4
Qluqlu3 <plksmcgof@gmail.com>
2021-12-18 11:58:41 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from 'styles/Home.module.css';
+import { Button } from 'components/Button';
+
+const Home: NextPage = () => {
+  const xxx = '';
+  return (
+    <div>
+      <Button></Button>
+    </div>
+  );
+};
+
+export default Home;
